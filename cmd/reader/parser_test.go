fe5f9773cc4611ccd534ebad26d0ea9b81c674d3
Phillip Theriault <phillip.theriault@spellweaver.com>
2025-08-25 17:18:45 -0600

    Changed test strings to be more readable

--- cmd/reader/parser_test.go
+++ cmd/reader/parser_test.go
@@ -12,34 +12,34 @@ func TestRowParser(t *testing.T) {
 
 	// Test: Valid single row
 	log.Println("Testing valid single row...")
-	data := []byte("One,Two,Three,Four,Five")
+	data := []byte(`One,Two,Three,Four,Five`)
 	row := parseLine(data)
 	row.Print()
 	assert.Equal(t, 5, row.columnCount)
-	assert.Equal(t, []string{"One", "Two", "Three", "Four", "Five"}, row.columns)
+	assert.Equal(t, []string{`One`, `Two`, `Three`, `Four`, `Five`}, row.columns)
 
 	// Test: Valid field with a comma
 	log.Println("Testing valid field with a comma...")
-	data = []byte("One,Two,Three,Four,\"Five, Six\"")
+	data = []byte(`One,Two,Three,Four,"Five, Six"`)
 	row = parseLine(data)
 	row.Print()
 	assert.Equal(t, 5, row.columnCount)
-	assert.Equal(t, []string{"One", "Two", "Three", "Four", "Five, Six"}, row.columns)
+	assert.Equal(t, []string{`One`, `Two`, `Three`, `Four`, `Five, Six`}, row.columns)
 
 	// Test: Valid field with comma and double quotes at beginning
 	log.Println("Testing valid field with comma and double quotes...")
-	data = []byte("\"One, \"\"Two\"\"\",Three,Four,Five,Six")
+	data = []byte(`"One, ""Two""",Three,Four,Five,Six`)
 	row = parseLine(data)
 	row.Print()
 	assert.Equal(t, 5, row.columnCount)
-	assert.Equal(t, []string{"One, \"Two\"", "Three", "Four", "Five", "Six"}, row.columns)
+	assert.Equal(t, []string{`One, "Two"`, `Three`, `Four`, `Five`, `Six`}, row.columns)
 
 	// Test: Valid field with comma and double quotes at beginning and end
 	log.Println("Testing valid fields with commas and double quotes...")
-	data = []byte("\"\"\"One, Two\"\", Three\",Four,\"Five, \"\"Six\"\"\"")
+	data = []byte(`"""One, Two"", Three",Four,"Five, ""Six"""`)
 	row = parseLine(data)
 	row.Print()
 	assert.Equal(t, 3, row.columnCount)
-	assert.Equal(t, []string{"\"One, Two\", Three", "Four", "Five, \"Six\""}, row.columns)
+	assert.Equal(t, []string{`"One, Two", Three`, `Four`, `Five, "Six"`}, row.columns)
 
 }
